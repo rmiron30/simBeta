edbf3a325566ab1141295604842b3f92c06c47b4
rmiron30 <raluca.miron@eli-np.ro>
2024-08-05 17:45:38 +0200

    PhysicsList.cc is updated with VITO physics and copy contains the previous physics

--- B3a/src/PhysicsList.cc
+++ B3a/src/PhysicsList.cc
@@ -1,64 +1,43 @@
+// $Id: PhysicsList.cc 68752 2016-11-28 A. Fijalkowska $
 //
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
+/// \file PhysicsList.cc
+/// \brief Implementation of the PhysicsList class based on optical/LXe/LXePhysicsList 
 //
 //
-/// \file B3/B3a/src/PhysicsList.cc
-/// \brief Implementation of the B3::PhysicsList class
-
 #include "PhysicsList.hh"
+#include "GeneralPhysics.hh"
+#include "EMPhysics.hh"
+#include "G4SystemOfUnits.hh"
 
-#include "G4DecayPhysics.hh"
-#include "G4EmStandardPhysics.hh"
-#include "G4RadioactiveDecayPhysics.hh"
+using namespace B3;
 
 namespace B3
 {
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-PhysicsList::PhysicsList()
+PhysicsList::PhysicsList() : G4VModularPhysicsList()
 {
-  SetVerboseLevel(1);
+  // default cut value  (1.0mm)
+  defaultCutValue = 0.000001 * mm;
 
-  // Default physics
-  RegisterPhysics(new G4DecayPhysics());
+  // General Physics
+  RegisterPhysics( new GeneralPhysics("general") );
 
-  // EM physics
-  RegisterPhysics(new G4EmStandardPhysics());
-
-  // Radioactive decay
-  RegisterPhysics(new G4RadioactiveDecayPhysics());
+  // EM Physics
+  RegisterPhysics( new EMPhysics("standard EM"));
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-void PhysicsList::SetCuts()
-{
-  G4VUserPhysicsList::SetCuts();
+PhysicsList::~PhysicsList() {}
+
+
+void PhysicsList::SetCuts(){
+  //  " G4VUserPhysicsList::SetCutsWithDefault" method sets
+  //   the default cut value for all particle types
+  SetCutsWithDefault();
 }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+}
 
-}  // namespace B3
+
+
+
